718fe8ebfbf1a1ff5ab3ecb91207a4390256d26f
bosik2704 <bosik2704@gmail.com>
2025-03-14 17:19:08 +0900

    20250314_1519 JspringProject 초기

---
+++ src/main/java/com/spring/JspringProject/controller/StudyController.java
@@ -0,0 +1,71 @@
+package com.spring.JspringProject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.JspringProject.service.StudyService;
+
+//@RestController
+@Controller
+@RequestMapping("/study")
+public class StudyController {
+
+	@Autowired
+	private StudyService studyService;
+	
+	@RequestMapping("/ajax/ajaxForm")
+	public String ajaxFormGet() {
+		return "study/ajax/ajaxForm";
+	}
+	
+//  동기식 호출
+//	@RequestMapping("/ajax/ajaxTest1")
+//	public String ajaxTest1Get(Model model, int idx) {
+//		model.addAttribute("idx", idx);
+//		return "study/ajax/ajaxForm";
+//	}
+	
+	@ResponseBody
+	@RequestMapping(value="/ajax/ajaxTest1", method = RequestMethod.POST, produces="application/text; charset=utf-8")
+//	@RequestMapping(value="/ajax/ajaxTest1", method = RequestMethod.POST)
+	public String ajaxTest1Post(int idx) {
+		String str = "전송값 : " + idx;
+		return str;
+	}
+	
+	@RequestMapping(value="/ajax/ajaxTest2_1", method = RequestMethod.GET)
+	public String ajaxTest2_1Get() {
+		return "study/ajax/ajaxTest2_1";
+	}
+	
+//	@ResponseBody
+//	@RequestMapping(value="/ajax/ajaxTest2_1", method = RequestMethod.POST)
+//	public String[] ajaxTest2_1Post(String dodo) {
+//		String[] strArray = new String[100];
+//		strArray = studyService.getCityStringArray(dodo);
+//		return strArray;
+//	}
+	
+	@ResponseBody
+	@RequestMapping(value="/ajax/ajaxTest2_1", method = RequestMethod.POST)
+	public String[] ajaxTest2_1Post(String dodo) {
+		return studyService.getCityStringArray(dodo);
+	}
+	
+	@RequestMapping(value="/ajax/ajaxTest2_2", method = RequestMethod.GET)
+	public String ajaxTest2_2Get() {
+		return "study/ajax/ajaxTest2_2";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/ajax/ajaxTest2_2", method = RequestMethod.POST)
+	public List<String> ajaxTest2_2Post(String dodo) {
+		return studyService.getCityVosArray(dodo);
+	}
+}
---
+++ src/main/java/com/spring/JspringProject/dao/GuestDao.java
@@ -0,0 +1,19 @@
+package com.spring.JspringProject.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.spring.JspringProject.vo.GuestVo;
+
+public interface GuestDao {
+
+	List<GuestVo> getGuestList(@Param("startIndexNo") int startIndexNo, @Param("pageSize") int pageSize);
+
+	int setGuestInput(@Param("vo") GuestVo vo);
+
+	int setGuestDelete(@Param("idx") int idx);
+
+	int getTotRecCnt();
+
+}
